ffb0d5c914f5c4d6a37dd6ba6a491a69a6f458a4
lusacramento <lucianossacramento@yahoo.com.br>
2025-08-12 13:54:10 -0300

    update app

--- app.vue
+++ app.vue
@@ -1,39 +1,59 @@
 <template>
-  <div label="Microphone">
-    <button @click="startAudio" ref="micButton">Tone</button>
-    <div id="content">
-    </div>
+  <div>
+    <button ref="startButton" disabled @click.prevent="startRecord">Gravar</button>
+    Meter: {{ meterValue }}
   </div>
 </template>
 
 <script lang="ts" setup>
-import { UserMedia, Volume, start, FFT, Offline } from 'tone';
-const mic = ref() as Ref<UserMedia>
-const micFFT = ref() as Ref<FFT>
-const micButton = ref()
+import { UserMedia, Meter } from 'tone';
 
-async function startAudio() {
-  try {
-    await mic.value.open()
-    mic.value.connect(micFFT.value);
-    
-  } catch (error) {
-    console.error(error)
-  }
+const startButton = ref()
+const meterValue = ref()
+
+
+onMounted(() => {
+  startButton.value.disabled = false
+})
+
+
+function startRecord() {
+  console.log("start record");
+  startButton.value.disabled = true
+
+
+  const meter = new Meter()
+  const mic = new UserMedia()
+  mic.connect(meter)
+  mic.open().then(() => {
+    // promise resolves when input is available
+    console.log("mic open");
+    // print the incoming mic levels in decibels
+    setInterval(() => meterValue.value = meter.getValue(), 200);
+  }).catch(e => {
+    // promise is rejected when the user doesn't have or allow mic access
+    console.log("mic not open");
+  });
+
+
+
+
+
 }
 
-onMounted(async () => {
-  mic.value = new UserMedia()
-  micFFT.value = new FFT()
-  
-})
+function stopRecord() {
+  stopButton.value.disabled = true
+  startButton.value.disabled = false
+}
+
 </script>
-<style>
-#content{
+<style scoped>
+/* #content{
 width: 100%;
   background-color: black;
   height: 40px;
   border-radius: 20px;
   margin-bottom: 10px;
 }
+*/
 </style>
